9360c18ed4eef8246542e7f85df136b10607ddec
Daerdemandt <daerdemandt@gmail.com>
2015-05-12 20:47:44 +0300

    Improved readability

--- Tree.py
+++ Tree.py
@@ -12,17 +12,16 @@ class DimTreeNode(object):
 			return isinstance(obj, list) or isinstance(obj, tuple)
 		assert is_ordered(limits)
 		self.dimensions = len(limits)
-		assert all(is_ordered(limits_pair) and len(limits_pair) == 2 for limits_pair in limits)
-		assert all(lim_pair[1] > lim_pair[0] for lim_pair in limits)
+		def is_limit_pair(obj):
+			return is_ordered(obj) and len(obj) == 2 and obj[1] > obj[0]
+		assert all(is_limit_pair(limit_pair) for limit_pair in limits)
 		self.limits = tuple(limits)
 		self.objects = []
 		self.is_leaf = True
 		self.capacity = node_capacity
 		
 	def volume_contains(self, obj):
-		def coord_is_in(obj_coord, coord_limits):
-			return coord_limits[0] <= obj_coord <= coord_limits[1]
-		return all(coord_is_in(obj[i], self.limits[i]) for i in range(self.dimensions))
+		return is_in_box(obj, self.limits)
 	
 	def accomodation(self, obj):
 		assert self.volume_contains(obj)
@@ -76,11 +75,15 @@ class DimTreeNode(object):
 			
 	def print_recursive(self, level=0):
 		if level == 0:
-			print('Dimensions = ', self.dimensions)
 			print('Capacity = ', self.capacity)
 		print('\t' * level + 'Limits:', self.limits)
+		
+		def object_to_str(obj): # replace 2 in {:0.2g} for more digits
+			numbers = '; '.join('{:0.2g}'.format(num) for num in obj)
+			return '(' + numbers + ')'
+		
 		if self.is_leaf:
-			print('\t' * level + 'Contents:', ' '.join(str(obj) for obj in self.objects))
+			print('\t' * level + 'Contents:', ', '.join(object_to_str(obj) for obj in self.objects))
 		else:
 			print('\t' * level + 'Descendants:')
 			for d in self.descendants:
@@ -98,50 +101,81 @@ class DimTreeNode(object):
 			raise StopIteration
 		child_result_iterators = [self.descendants[child].get_nodes_by_predicate(pred) for child in self.descendants]
 		yield from chain.from_iterable(child_result_iterators)
+	
+	def get_objects_by_node_predicate(self, pred):
+		for leaf in self.get_nodes_by_predicate(pred):
+			yield from leaf.objects
 
 # End of DimTreeNode
 
+def check_dimensions(obj1, obj2):
+	dimensions = len(obj1)
+	assert len(obj2) == dimensions
+	return dimensions 
 
-def distance_from_number_to_interval(number, interval):
-	if interval[0] <= number <= interval[1]:
+def is_in_interval(number, interval):
+	return interval[0] <= number <= interval[1]
+
+def distance_to_interval(number, interval):
+	if is_in_interval(number, interval):
 		return 0
 	return min(abs(number - border) for border in interval)
+	
+def is_in_box(point_coords, box_coords):
+	dimensions = check_dimensions(point_coords, box_coords)
+	return all(is_in_interval(point_coords[i], box_coords[i]) for i in range(dimensions))
 
 def distance_to_box(point_coords, box_coords):
-	dimensions = len(point_coords)
-	assert len(box_coords) == dimensions
+	dimensions = check_dimensions(point_coords, box_coords)
 	
 	def distance_component(index):
-		return distance_from_number_to_interval(point_coords[index], box_coords[index])
+		return distance_to_interval(point_coords[index], box_coords[index])
 	return sum(distance_component(i) for i in range(dimensions))
 
 def sphere_intersects_with_box(center, radius, box):
 	return radius >= distance_to_box(center, box)
 		
 
-def show_usage():
+def usage_example():
+
+	def generate_point():
+		def random_coordinate(limits):
+			return limits[0] + random() * (limits[1] - limits[0])
+		return tuple(random_coordinate(coord_limit) for coord_limit in volume_limits)	
+			
+
+	def print_points(points):
+		def point_to_str(obj): # replace 2 in {:0.2g} for more digits
+			numbers = '; '.join('{:0.2g}'.format(num) for num in obj)
+			return '(' + numbers + ')'
+		print(', '.join(point_to_str(point) for point in points))
+
+	# Tree operates on hyperrectangle, or box. Let's show on one-dimensional example:
 	maximum_x = 1
 	minimum_x = 0
 	x_limits = (minimum_x, maximum_x)
 	volume_limits = (x_limits,)
-	
-	def generate_point():
-		def random_coordinate(limits):
-			return limits[0] + random() * (limits[1] - limits[0])
-		return tuple(random_coordinate(coord_limit) for coord_limit in volume_limits)
 
 	example_tree = DimTreeNode(volume_limits, node_capacity=2)
 	
+	# Adding points. Can be done one by one
 	number_of_points = 5
 	for i in range(number_of_points // 2):
 		example_tree.add(generate_point())
-	# or
+	# or by list
 	points = [generate_point() for i in range(number_of_points // 2, number_of_points)]
 	example_tree.add_list(points)
-		
-	example_tree.print_recursive()
-	print('All points:', list(example_tree.traverse_objects()))
 	
+	# Let's look at the whole tree:
+	example_tree.print_recursive()
+	
+	# Or iterate through all the points. Also, note that they're sorted by x
+	print('All points:')
+	print_points(example_tree.traverse_objects())
+	
+	# Getting nodes and objects by box-predicate:
+	nodes_by_predicate = example_tree.get_nodes_by_predicate
+	objects_by_predicate = example_tree.get_objects_by_node_predicate
 	
 	def x_is_more_than_one_third(box):
 		return box[0][1] > 1.0/3
@@ -149,17 +183,17 @@ def show_usage():
 	def x_is_around_one_third(box):
 		return box[0][0] <= 1.0 / 3 <= box[0][1]
 		
-	nodes_more_than_third = example_tree.get_nodes_by_predicate(x_is_more_than_one_third)
-	nodes_around_third = example_tree.get_nodes_by_predicate(x_is_around_one_third)
-	
-	print("\nNodes corresponding to x > 1/3")
-	for node in nodes_more_than_third:
+	print("\nNodes corresponding to x > 1/3 :")
+	for node in nodes_by_predicate(x_is_more_than_one_third):
 		node.print_recursive()
-	print("\nNodes corresponding to x = 1/3")
-	for node in nodes_around_third:
+		
+	print("\nNodes corresponding to x = 1/3 :")
+	for node in nodes_by_predicate(x_is_around_one_third):
 		node.print_recursive()
 	
+	print("\nPoints around x = 1/3 :")
+	print_points(objects_by_predicate(x_is_around_one_third))
 	
 	
 	
-show_usage()
+usage_example()
